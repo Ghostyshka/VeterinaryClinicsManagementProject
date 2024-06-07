92aef899ca56b6bf1181fda17586c3caa21231c6
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-07 16:17:19 +0300

    refactored: deleted useless part of code

--- VetClinicManagament/VetClinicsDomain/Data/DataContext.cs
+++ VetClinicManagament/VetClinicsDomain/Data/DataContext.cs
@@ -9,11 +9,6 @@ public class DataContext : DbContext
     {
         
     }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=admin;Database=VetClinic;Trust Server Certificate=true;");
-    }
-
 
     public DbSet<User> User { get; set; }
 }
