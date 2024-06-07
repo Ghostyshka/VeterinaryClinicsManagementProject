0cf67ad81d328bda18d11fa445638443672bfc08
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-07 16:18:21 +0300

    updated: added needed characteristics for Animal entity

--- VetClinicManagament/VetClinicsDomain/Entity/Animal.cs
+++ VetClinicManagament/VetClinicsDomain/Entity/Animal.cs
@@ -4,4 +4,13 @@ public class Animal
 {
     public int AnimalId { get; set; }
     public string AnimalName { get; set; }
+    public string Species { get; set; }
+    public int Age { get; set; }
+    public string Breed { get; set; }
+    public string Color { get; set; }
+    public string Gender { get; set; }
+    public bool Health { get; set; }
+    public bool Vaccination { get; set; }
+    public string VaccineType { get; set; }
+    public double Weight { get; set; }
 }
