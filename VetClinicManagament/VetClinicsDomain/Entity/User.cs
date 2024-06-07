1c1be27abbd71442357838e8ba5ccabcbd87294a
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-07 16:21:33 +0300

    refactored: for correct type

--- VetClinicManagament/VetClinicsDomain/Entity/User.cs
+++ VetClinicManagament/VetClinicsDomain/Entity/User.cs
@@ -5,7 +5,7 @@ public class User
     public int UserId { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    public int DateOfBirth { get; set; }                // TO DO: implement happy birthday notifications
+    public DateTime DateOfBirth { get; set; }                // TO DO: implement happy birthday notifications
 
     public string Email { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
