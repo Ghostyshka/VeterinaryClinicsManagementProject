a9fc399a41f4a5304ba2a00b57af89d3dca530ab
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-12 19:02:44 +0300

    updated: User entity

--- VetClinicManagament/VetClinicsDomain/Entity/User.cs
+++ VetClinicManagament/VetClinicsDomain/Entity/User.cs
@@ -1,17 +1,33 @@
-﻿namespace VetClinicsDomain.Entity;
+﻿using System;
+using VetClinic.Domain.Enum;
+
+namespace VetClinic.Domain.Entity;
 
 public class User
 {
+    private UserRole _userRole;
+
     public int UserId { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    public DateTime DateOfBirth { get; set; }                // TO DO: implement happy birthday notifications
+    public DateTime DateOfBirth { get; set; } // TO DO: implement happy birthday notifications
+
+    public UserRole UserRole
+    {
+        get => _userRole;
+        set
+        {
+            if (!System.Enum.IsDefined(typeof(UserRole), value))
+            {
+                throw new ArgumentException("Invalid user role");
+            }
+            _userRole = value;
+        }
+    }
 
     public string Email { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
-
     public string Password { get; set; } = string.Empty;
-
     public DateTime CreatedAt { get; set; }
 
     public ICollection<UserAnimal> UserAnimals { get; set; } = new List<UserAnimal>();
