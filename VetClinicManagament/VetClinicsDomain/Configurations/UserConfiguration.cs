4d932ef351d099c7a01793e1a775971637d69343
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-12 14:39:52 +0300

    added: Configuration for entity's

---
+++ VetClinicManagament/VetClinicsDomain/Configurations/UserConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VetClinicsDomain.Entity;
+
+namespace VetClinic.Domain.Configurations;
+
+internal class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.UserId);
+
+        builder.Property(u => u.FirstName)
+            .HasMaxLength(128)
+            .IsRequired();
+
+        builder.Property(u => u.LastName)
+            .HasMaxLength(128)
+            .IsRequired();
+
+        builder.HasIndex(u => u.Email)
+            .IsUnique();
+
+        builder.Property(u => u.PhoneNumber)
+            .HasMaxLength(15);
+
+        builder.Property(u => u.Password)
+            .HasMaxLength(256)
+            .IsRequired();
+
+        builder.Property(u => u.DateOfBirth)
+            .IsRequired();
+
+        builder.Property(u => u.CreatedAt)
+            .IsRequired();
+
+        builder.Property(u => u.UserRole)
+            .IsRequired();
+
+    }
+}
--- VetClinicManagament/VetClinicsDomain/Data/DataContext.cs
+++ VetClinicManagament/VetClinicsDomain/Data/DataContext.cs
@@ -7,8 +7,14 @@ public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
-        
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
     }
 
-    public DbSet<User> User { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Animal> Animals { get; set; }
+    public DbSet<UserAnimal> UserAnimals { get; set; }
+
 }
