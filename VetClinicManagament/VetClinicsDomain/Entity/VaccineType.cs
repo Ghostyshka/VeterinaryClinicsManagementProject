a344a2cf7c8a130465097c817d22fa677f56adb7
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-12 15:21:10 +0300

    added: Vaccine and VaccineType entity's

---
+++ VetClinicManagament/VetClinicsDomain/Entity/VaccineType.cs
@@ -0,0 +1,16 @@
+﻿namespace VetClinic.Domain.Entity;
+
+internal class VaccineType
+{
+    public int VaccineTypeId { get; set; }
+    public string LiveAttenuated { get; set; }
+    public string Inactivated { get; set; }
+    public string Toxoid { get; set; }
+    public string Subunit { get; set; }
+    public string Recombinant { get; set; }
+    public string Conjugate { get; set; }
+    public string DNARNA { get; set; }
+    public string Adjuvanted { get; set; }
+    public string Multivalent { get; set; }
+    public string Vector { get; set; }
+}
