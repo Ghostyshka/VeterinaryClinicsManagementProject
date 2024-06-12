6f6f7c251ff85a7c50b2f0ef566095e0e78762d9
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-12 18:05:23 +0300

    updated: using and some refactor

--- VetClinicManagament/Core/Repositories/UserRepository.cs
+++ VetClinicManagament/Core/Repositories/UserRepository.cs
@@ -1,6 +1,5 @@
 ﻿using System.Data;
 using VetClinic.Core.Repositories.Base;
-using VetClinicsDomain.Entity;
 namespace VetClinic.Core.Repositories;
 
 
@@ -9,16 +8,7 @@ public class UsersRepository : BaseRepository
     public UsersRepository(IDbTransaction transaction)
       : base(transaction)
     {
+
+
     }
-
-
-    /*public async Task<int> AddUserAsync(User newUser)
-    {
-        return await Connection.ExecuteScalarAsync<int>(
-            @"INSERT INTO [Users](FullName, UserName, Password, Email, PhoneNumber)
-                VALUES (@FullName, @UserName, @Password, @Email, @PhoneNumber);",
-            param: new { Fullname = newUser.FullName, UserName = newUser.UserName, Password = newUser.Password, Email = newUser.Email, PhoneNumber = newUser.PhoneNumber },
-            transaction: Transaction
-        );
-    }*/
 }
