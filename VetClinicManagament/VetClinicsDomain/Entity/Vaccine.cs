3df73892cb064b91db3f0481f3519aac53818988
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-12 18:07:42 +0300

    refactored: access modifier and naming

--- VetClinicManagament/VetClinicsDomain/Entity/Vaccine.cs
+++ VetClinicManagament/VetClinicsDomain/Entity/Vaccine.cs
@@ -1,10 +1,10 @@
 ﻿namespace VetClinic.Domain.Entity;
 
-internal class Vaccine
+public class Vaccine
 {
     public int VaccineId { get; set; }
-    public string Name { get; set; }
-    public VaccineType Type { get; set; }
+    public string VaccineName { get; set; }
+    public VaccineType VaccineType { get; set; }
     public string Manufacturer { get; set; }
     public DateTime ExpiryDate { get; set; }
 }
