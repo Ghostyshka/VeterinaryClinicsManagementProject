e912e6f9c2d83a66981a923652bb1ae92569110e
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-12 18:06:10 +0300

    added: Vaccine and VaccineType configurations

---
+++ VetClinicManagament/VetClinicsDomain/Configurations/VaccineConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VetClinic.Domain.Entity;
+
+namespace VetClinic.Domain.Configurations;
+
+internal class VaccineConfiguration : IEntityTypeConfiguration<Vaccine>
+{
+    public void Configure(EntityTypeBuilder<Vaccine> builder)
+    {
+        builder.HasKey(v => v.VaccineId);
+
+        builder.Property(v => v.VaccineName)
+            .HasMaxLength(128)
+            .IsRequired();
+
+        builder.Property(v => v.VaccineType)
+            .HasMaxLength(128)
+            .IsRequired();
+
+        builder.Property(v => v.Manufacturer)
+            .HasMaxLength(128)
+            .IsRequired();       
+        
+        builder.Property(v => v.ExpiryDate)
+            .IsRequired();
+    }
+}
