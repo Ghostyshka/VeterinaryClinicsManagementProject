1c8604a6faf57e626146fef9c5424ed497bc6fbb
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-12 18:03:21 +0300

    created: Base repo

---
+++ VetClinicManagament/Core/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace VetClinic.Core.Repositories.Base;
+
+public abstract class BaseRepository
+{
+    protected IDbTransaction Transaction { get; private set; }
+    protected IDbConnection Connection { get { return Transaction.Connection; } }
+
+    public BaseRepository(IDbTransaction transaction)
+    {
+        Transaction = transaction;
+    }
+}
