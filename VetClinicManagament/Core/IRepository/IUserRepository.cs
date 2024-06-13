d3ce86eea1f84c36dc3c0b1ed0f99323a1d6de52
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-13 18:53:14 +0300

    added: User repo and interface

---
+++ VetClinicManagament/Core/IRepository/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using VetClinic.Domain.Models;
+using VetClinic.Domain.Models.Dtos;
+
+namespace VetClinic.Core.IRepository;
+
+public interface IUserRepository
+{
+    Task<int> AddUserAsync(UserDto newUser);
+    Task<UserModel> GetUserByUsernameAsync(string userName);
+}
--- VetClinicManagament/Core/Repositories/UserRepository.cs
+++ VetClinicManagament/Core/Repositories/UserRepository.cs
@@ -1,14 +1,50 @@
-﻿using System.Data;
+﻿using Dapper;
+using System.Data;
+using VetClinic.Core.IRepository;
 using VetClinic.Core.Repositories.Base;
+using VetClinic.Domain.Models;
+using VetClinic.Domain.Models.Dtos;
+
 namespace VetClinic.Core.Repositories;
 
-
-public class UsersRepository : BaseRepository
+public class UserRepository : BaseRepository, IUserRepository
 {
-    public UsersRepository(IDbTransaction transaction)
-      : base(transaction)
+    public UserRepository(IDbTransaction transaction)
+        : base(transaction)
     {
-
-
     }
+
+    public async Task<int> AddUserAsync(UserDto newUser)
+    {
+        return await Connection.ExecuteScalarAsync<int>(
+            @"INSERT INTO [Users](FirstName, LastName, Username, DateOfBirth, UserRole, Email, PhoneNumber, Password, CreatedAt)
+                  VALUES (@FirstName, @LastName, @Username, @DateOfBirth, @UserRole, @Email, @PhoneNumber, @Password, @CreatedAt);
+                  SELECT CAST(SCOPE_IDENTITY() as int);",
+            param: new
+            {
+                newUser.FirstName,
+                newUser.LastName,
+                newUser.Username,
+                newUser.DateOfBirth,
+                newUser.UserRole,
+                newUser.Email,
+                newUser.PhoneNumber,
+                newUser.Password,
+                newUser.CreatedAt
+            },
+            transaction: Transaction
+        );
+    }
+
+    public async Task<UserModel> GetUserByUsernameAsync(string userName)
+    {
+        var user = await Connection.QueryFirstOrDefaultAsync<UserModel>(
+            @"SELECT Password AS PasswordHash, UserName, UserId  FROM [Users] WHERE UserName = @Username",
+            param: new { Username = userName },
+            transaction: Transaction
+        );
+
+        return user;
+    }
+
 }
