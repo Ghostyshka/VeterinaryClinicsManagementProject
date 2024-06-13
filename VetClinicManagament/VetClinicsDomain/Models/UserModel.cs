39b38183b8100e3ee4e611cbced77f8990ed2f0b
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-13 18:33:01 +0300

    added: UserModel and UserDto, UserLoginDto

---
+++ VetClinicManagament/VetClinicsDomain/Models/UserModel.cs
@@ -0,0 +1,32 @@
+﻿using VetClinic.Domain.Enum;
+
+namespace VetClinic.Domain.Models;
+
+public class UserModel
+{
+    private UserRole _userRole;
+
+    public int UserId { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Username => $"{FirstName} {LastName}";
+    public DateTime DateOfBirth { get; set; } // TO DO: implement happy birthday notifications
+
+    public UserRole UserRole
+    {
+        get => _userRole;
+        set
+        {
+            if (!System.Enum.IsDefined(typeof(UserRole), value))
+            {
+                throw new ArgumentException("Invalid user role");
+            }
+            _userRole = value;
+        }
+    }
+
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
