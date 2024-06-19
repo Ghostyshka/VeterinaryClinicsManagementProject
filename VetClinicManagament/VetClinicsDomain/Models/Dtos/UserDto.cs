0049af86f8044dbc3e38df0f0cbf65bdccc5776b
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-19 20:41:34 +0300

    added: PasswordHash to UserDto

--- VetClinicManagament/VetClinicsDomain/Models/Dtos/UserDto.cs
+++ VetClinicManagament/VetClinicsDomain/Models/Dtos/UserDto.cs
@@ -44,5 +44,6 @@ public class UserDto
     public string Email { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
 }
