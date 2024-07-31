bd5c3ce1f2524020703929a25fbf63be9e3271e1
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-07-31 16:39:24 +0300

    added: needed using for [Authorize]

--- VetClinicManagament/Presentation/Controllers/ClientController.cs
+++ VetClinicManagament/Presentation/Controllers/ClientController.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using Domain.Models.Dtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
