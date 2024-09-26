dc1251bbdc67ae878974dc70523fd53cc10f5d05
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-09-26 17:14:00 +0300

    refactored: person models

--- VetClinicManagament/Core/Domain/Models/EmployeeModel.cs
+++ VetClinicManagament/Core/Domain/Models/EmployeeModel.cs
@@ -1,14 +1,6 @@
 ﻿namespace Domain.Models;
 
-public class EmployeeModel
+public class EmployeeModel : PersonModel
 {
     public int EmployeeId { get; set; }
-    public string FullName { get; set; } = string.Empty;
-    public DateTime DateOfBirth { get; set; } // TO DO: implement happy birthday notifications
-
-    public string Email { get; set; } = string.Empty;
-    public string PhoneNumber { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; }
 }
