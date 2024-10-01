98aba0bd5f73a69c5cf5f19777de4731555eb185
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-10-01 16:21:48 +0300

    added: dto for updating animal

---
+++ VetClinicManagament/Core/Domain/Models/Dtos/AnimalUpdateDto.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Enum;
+
+namespace Domain.Models.Dtos;
+
+public class AnimalUpdateDto
+{
+    private AnimalGender? _gender;
+
+    public string AnimalName { get; set; }
+    public int? SpeciesId { get; set; }
+    public int? ColorId { get; set; }
+    public int? BreedId { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+
+    public AnimalGender? AnimalGender
+    {
+        get => _gender;
+        set
+        {
+            if (value.HasValue && !System.Enum.IsDefined(typeof(AnimalGender), value.Value))
+            {
+                throw new ArgumentException("Invalid animal gender");
+            }
+            _gender = value;
+        }
+    }
+
+    public bool IsLive { get; set; }
+    public double? Weight { get; set; }
+}
