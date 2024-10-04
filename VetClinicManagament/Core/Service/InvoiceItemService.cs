1b6e7c07a53ac899289069de8afa6820217c0564
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-10-04 12:12:24 +0300

    refactored: used repositoryManager in service to navigate to the method in repo

--- VetClinicManagament/Core/Service/InvoiceItemService.cs
+++ VetClinicManagament/Core/Service/InvoiceItemService.cs
@@ -8,47 +8,47 @@ namespace Service;
 
 public class InvoiceItemService : IInvoiceItemService
 {
-    private readonly IInvoiceItemRepository _invoiceItemRepository;
+    private readonly IRepositoryManager _repositoryManager;
     private readonly IMapper _mapper;
 
-    public InvoiceItemService(IInvoiceItemRepository invoiceItemRepository, IMapper mapper)
+    public InvoiceItemService(IRepositoryManager repositoryManager, IMapper mapper)
     {
-        _invoiceItemRepository = invoiceItemRepository;
+        _repositoryManager = repositoryManager;
         _mapper = mapper;
     }
 
     public async Task<IEnumerable<InvoiceItemDto>> GetAllAsync()
     {
-        var invoiceItems = await _invoiceItemRepository.GetAllAsync();
+        var invoiceItems = await _repositoryManager.InvoiceItemRepository.GetAllAsync();
         return _mapper.Map<IEnumerable<InvoiceItemDto>>(invoiceItems);
     }
 
     public async Task<InvoiceItemDto> GetByIdAsync(int id)
     {
-        var invoiceItem = await _invoiceItemRepository.GetByIdAsync(id);
+        var invoiceItem = await _repositoryManager.InvoiceItemRepository.GetByIdAsync(id);
         return _mapper.Map<InvoiceItemDto>(invoiceItem);
     }
 
     public async Task<InvoiceItemDto> AddAsync(InvoiceItemDto invoiceItemDto)
     {
         var invoiceItem = _mapper.Map<InvoiceItem>(invoiceItemDto);
-        await _invoiceItemRepository.AddAsync(invoiceItem);
+        await _repositoryManager.InvoiceItemRepository.AddAsync(invoiceItem);
         return invoiceItemDto;
     }
 
     public async Task<InvoiceItemDto> UpdateAsync(InvoiceItemDto invoiceItemDto)
     {
         var invoiceItem = _mapper.Map<InvoiceItem>(invoiceItemDto);
-        await _invoiceItemRepository.UpdateAsync(invoiceItem);
+        await _repositoryManager.InvoiceItemRepository.UpdateAsync(invoiceItem);
         return invoiceItemDto;
     }
 
     public async Task<bool> DeleteAsync(int id)
     {
-        var item = await _invoiceItemRepository.GetByIdAsync(id);
+        var item = await _repositoryManager.InvoiceItemRepository.GetByIdAsync(id);
         if (item == null) return false;
 
-        await _invoiceItemRepository.DeleteAsync(id);
+        await _repositoryManager.InvoiceItemRepository.DeleteAsync(id);
         return true;
     }
 }
