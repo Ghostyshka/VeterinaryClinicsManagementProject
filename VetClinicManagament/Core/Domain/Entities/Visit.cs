f174613d9ec2a3c1cb8631353dd7a8d5767cd636
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-10-11 14:18:14 +0300

    deleted: useless comment's

--- VetClinicManagament/Core/Domain/Entities/Visit.cs
+++ VetClinicManagament/Core/Domain/Entities/Visit.cs
@@ -10,15 +10,12 @@ public class Visit
     public int UserId { get; set; }
     public User User { get; set; }
 
-    // Every visit should be assigned to an employee (doctor, vet, etc)
     public int EmployeeId { get; set; }
     public Employee Employee { get; set; }
 
-    // Nullable InvoiceId, because not every visit will have an invoice immediately
     public int? InvoiceId { get; set; }
     public Invoice Invoice { get; set; }
 
-    // Nullable TreatmentPlan for visits that might not require treatments
     public int? TreatmentId { get; set; }
     public TreatmentPlan TreatmentPlan { get; set; }
 
