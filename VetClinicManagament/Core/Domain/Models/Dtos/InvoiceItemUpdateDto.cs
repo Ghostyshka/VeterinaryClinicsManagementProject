c0e85cbb85ca6fafbf1070bea40e9156da5488b0
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-10-21 17:08:03 +0300

    added: Quantity and Price to InvoiceItemUpdateDto

--- VetClinicManagament/Core/Domain/Models/Dtos/InvoiceItemUpdateDto.cs
+++ VetClinicManagament/Core/Domain/Models/Dtos/InvoiceItemUpdateDto.cs
@@ -5,4 +5,6 @@ public class InvoiceItemUpdateDto
     public int ItemId { get; set; }
     public int InvoiceId { get; set; }
     public string ItemType { get; set; }
+    public int Quantity { get; set; }
+    public double Price { get; set; }
 }
