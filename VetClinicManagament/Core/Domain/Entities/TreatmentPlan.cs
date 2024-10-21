a63bbc92b465243360bfcb2ca205d35fa42c174f
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-10-21 16:58:34 +0300

    removed: useless code

--- VetClinicManagament/Core/Domain/Entities/TreatmentPlan.cs
+++ VetClinicManagament/Core/Domain/Entities/TreatmentPlan.cs
@@ -4,12 +4,9 @@ public class TreatmentPlan
 {
     public int PlanId { get; set; }
     public int TreatmentId { get; set; }
-    public int ServiceTypeId { get; set; }
     public DateTime StartOfTreatment { get; set; }
     public DateTime EndOfTreatment { get; set; }
     public bool InClinic { get; set; }
-
-    public ServiceType ServiceType { get; set; }
 
     public ICollection<Visit> Visit { get; set; }
     public ICollection<TreatmentPlanItem> TreatmentPlanItems { get; set; }
