129e2095bac153eb9457547b8589d6cdfc191927
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-10-24 12:49:03 +0300

    fixed: type for Dosage

--- VetClinicManagament/Core/Domain/Models/Dtos/TreatmentPlanItemDto.cs
+++ VetClinicManagament/Core/Domain/Models/Dtos/TreatmentPlanItemDto.cs
@@ -2,10 +2,10 @@
 
 public class TreatmentPlanItemDto
 {
-    public int PlanItemId { get; set; }
+    public int PlanId { get; set; }
     public int ServiceId { get; set; }
     public int MedicalId { get; set; }
     public string ItemDescription { get; set; }
-    public int Dosage { get; set; }
+    public double Dosage { get; set; }
     public int Quantity { get; set; }
 }
